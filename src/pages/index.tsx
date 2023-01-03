669b1c483d46dd92d5f03d8f518977599525c1a5
Brandon Wie <brandonwie.cs@gmail.com>
2023-01-03 18:55:20 +0900

    Add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import { trpc } from '@/utils/trpc';
+import { getOptionsForVote } from '../utils/getRandomPokemon';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: 'brandon' });
-  if (hello.isLoading) return <div>loading...</div>;
-  if (hello.data) return <div>{hello.data.greeting}</div>;
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which Pokémon is Rounder?</div>
       <div className='p-2' /> {/* better approach without side effects */}
       <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-        <div className='w-16 h-16 bg-red-200'> </div>
+        <div className='w-16 h-16 bg-red-800'>{first.toString()}</div>
         <div className='p-8'>VS</div>
-        <div className='w-16 h-16 bg-red-200'> </div>
+        <div className='w-16 h-16 bg-red-800'>{second.toString()}</div>
       </div>
     </div>
   );
