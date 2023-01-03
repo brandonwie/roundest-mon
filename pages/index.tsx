9cfd0bbb2fa31e8ec8f8fd6c03aab0210844dd7f
Brandon Wie <brandonwie.cs@gmail.com>
2023-01-03 15:47:31 +0900

    base uI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'>Which Pokémon is Rounder?</div>
+      <div className='p-2' /> {/* better approach without side effects */}
+      <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
+        <div className='w-16 h-16 bg-red-200'> </div>
+        <div className='p-8'>VS</div>
+        <div className='w-16 h-16 bg-red-200'> </div>
+      </div>
+    </div>
+  );
 }
